2ddf64a0a9a0f16d8227268a21335e6a47b7eb00
Max Lee <m.m.lee@student.saintkentigern.com>
2024-06-26 21:29:53 +1200

    26 june 2

--- app/login.jsx
+++ app/login.jsx
@@ -14,9 +14,18 @@ export default function LoginScreen() {
         style={{ width: '100%', height: '100%' }}
         resizeMode='cover'
       >
-        <View style={{ backgroundColor: 'rgba(0, 0, 0, 0.25)' }} className="flex-1 w-1/2 h-screen backdrop-blur-lg">
+        <View style={{ backgroundColor: 'rgba(0, 0, 0, 0.25)' }} className="items-center justify-center flex-1 w-1/2 h-screen backdrop-blur-lg">
+          <Text className="my-4 text-4xl font-bold text-center text-white">Welcome To NAME</Text>
+
+          <Pressable className="w-[25vw] border h-[5vh] text-black text-center border-white bg-white font-bold justify-center rounded-2xl drop-shadow-lg my-4">Sign in with Spotify</Pressable>
+          <Pressable className="w-[25vw] border h-[5vh] text-black text-center border-white bg-white font-bold justify-center rounded-2xl drop-shadow-lg my-4">Sign in with Google</Pressable>
+          <Pressable className="w-[25vw] border h-[5vh] text-black text-center border-white bg-white font-bold justify-center rounded-2xl drop-shadow-lg my-4">Sign in with Facebook</Pressable>
+          <Pressable className="w-[25vw] border h-[5vh] text-black text-center border-white bg-white font-bold justify-center rounded-2xl drop-shadow-lg my-4">Sign in with Apple</Pressable>
+
+
+
           <Link href="/">
-            <Pressable className="items-center justify-center text-lg font-bold text-white">Login</Pressable>
+            <Pressable className="items-center justify-center text-lg font-bold text-center text-white">Login</Pressable>
           </Link>
         </View>
       </ImageBackground>
